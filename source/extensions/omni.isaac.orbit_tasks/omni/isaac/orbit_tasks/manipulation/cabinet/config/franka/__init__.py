884e2b3f5bfac66344dd0e45c941059fce65d9ff
Jinseok Kim <jinseok.kim970@gmail.com>
2024-04-11 00:08:23 +0900

    Add script for cabinet env using skrl
    "Isaac-Open-Drawer-Franka-IK-Rel-v0" Env를 skrl의 PPO로 train 하는 코드 추가

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/cabinet/config/franka/__init__.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/cabinet/config/franka/__init__.py
@@ -76,6 +76,7 @@ gym.register(
         "env_cfg_entry_point": ik_rel_env_cfg.FrankaCabinetEnvCfg,
         "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.CabinetPPORunnerCfg,
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
     disable_env_checker=True,
 )
