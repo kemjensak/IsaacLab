cae20baf931dfedeae86ab2f95968d2816fd4a2b
Jinseok Kim <jinseok.kim970@gmail.com>
2024-05-06 11:05:49 +0900

    edit param

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/unstructured/mdp/observations.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/unstructured/mdp/observations.py
@@ -29,3 +29,18 @@ def object_position_in_robot_root_frame(
         robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], object_pos_w
     )
     return object_pos_b
+
+def object_pose_in_robot_root_frame(
+    env: RLTaskEnv,
+    robot_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+    object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
+) -> torch.Tensor:
+    """The pose of the object in the robot's root frame."""
+    robot: RigidObject = env.scene[robot_cfg.name]
+    object: RigidObject = env.scene[object_cfg.name]
+    object_pos_w = object.data.root_pos_w[:, :3]
+    object_quat_w = object.data.root_quat_w[:, :4]
+    object_pos_b, object_quat_b = subtract_frame_transforms(
+        robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], object_pos_w, object_quat_w
+    )
+    return torch.concat((object_pos_b, object_quat_b), dim=1)
