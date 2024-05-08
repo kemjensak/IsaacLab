288f7a4466ce050f7c79e110ba7a866220b82bc7
Jinseok Kim <jinseok.kim970@gmail.com>
2024-05-08 22:15:12 +0900

    Add save/load function of object's pose dataset
    object들의 안정된 pose set의 저장과 로드를 위한 event term 추가 및 관련 코드 작성

---
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/unstructured/mdp/events.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING
+
+import omni.isaac.orbit.utils.math as math_utils
+from omni.isaac.orbit.assets import RigidObject, Articulation
+from omni.isaac.orbit.managers import SceneEntityCfg
+from omni.isaac.orbit.sensors import FrameTransformer
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from omni.isaac.orbit.envs import RLTaskEnv, BaseEnv
+
+
+def save_object_pose(
+    env: RLTaskEnv,
+    minimal_height: float,
+    object_cfg: list[SceneEntityCfg],
+):
+    asset: RigidObject | Articulation = env.scene[object_cfg[0].name]
+    # get default root state
+    root_states = asset.data.default_root_state.clone()
+
+    """Save pose of object on the table ."""
+    if env.episode_length_buf[0] != 250:
+        return
+    loaded_object_poses = torch.empty(1,8,7).to('cuda')
+    try:
+        loaded_object_poses = torch.from_numpy(np.load("/home/kjs-dt/RL/objcet_pose/object_poses.npy")).to('cuda')
+    except:
+        print("No file")
+
+    all_object_poses = torch.stack([env.scene[object_cfg_idx.name].data.root_state_w[:, :7]
+                                            for object_cfg_idx in object_cfg], dim=1)
+    
+    
+    all_object_poses[:, :, :3] -= (env.scene.env_origins + root_states[:, 0:3]).unsqueeze(1)
+    
+    for env_idx in all_object_poses:
+        object_dropped = False
+        for object_idx in env_idx:
+            if object_idx[2] < -0.06:
+                object_dropped = True
+                break
+        if object_dropped:
+            continue
+        loaded_object_poses = torch.concat((loaded_object_poses, env_idx.unsqueeze(0)), dim=0)
+
+    print(loaded_object_poses.shape)
+    object_poses_np = loaded_object_poses.cpu().numpy()
+    np.save("/home/kjs-dt/RL/objcet_pose/object_poses.npy", object_poses_np)
+
+def reset_root_state_from_file(
+    env: BaseEnv,
+    env_ids: torch.Tensor,
+    pose_range: dict[str, tuple[float, float]],
+    velocity_range: dict[str, tuple[float, float]],
+    asset_cfg: list[SceneEntityCfg],
+):
+    #Reset the asset root state to a random position and velocity uniformly within the given ranges.
+
+    # extract the used quantities (to enable type-hinting)
+    asset: RigidObject | Articulation = env.scene[asset_cfg[0].name]
+    # get default root state
+    root_states = asset.data.default_root_state.clone()
+
+    loaded_object_poses = torch.from_numpy(np.load("/home/kjs-dt/RL/objcet_pose/object_poses.npy")).to('cuda')
+    # (env, object, pose/ num_envs, 8, 7)
+    # 뽑은것들에서 object pose 가져오기
+    # 가져온 object pose들을 각 object에 적용
+
+    # episode(env) 갯수만큼 랜덤하게 뽑고, idx 저장
+    env_indices = torch.randperm(len(loaded_object_poses))[:env.num_envs]
+
+    #각 env에 대해 object pose를 적용
+    for object_idx in range(len(asset_cfg)):
+        object_poses = loaded_object_poses[env_indices, object_idx, :]
+        object_poses[:, :3] += (env.scene.env_origins + root_states[:, 0:3])
+        asset = env.scene[asset_cfg[object_idx].name]
+        asset.write_root_pose_to_sim(object_poses)
+
