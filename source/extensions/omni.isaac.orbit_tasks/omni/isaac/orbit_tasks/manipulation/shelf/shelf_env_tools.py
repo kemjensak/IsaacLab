d11df133f68012f4d27b03767662675288443d55
IROL-Kimtaeho <taeho.kim159@gmail.com>
2024-05-21 17:34:37 +0900

    shelf/shelf_env_tools.py 추가

---
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/shelf/shelf_env_tools.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from dataclasses import MISSING
+
+
+from omni.isaac.orbit.assets import RigidObjectCfg
+from omni.isaac.orbit.sim.spawners.from_files.from_files_cfg import UsdFileCfg
+from omni.isaac.orbit.utils import configclass
+from omni.isaac.orbit.utils.assets import ISAAC_NUCLEUS_DIR
+from omni.isaac.orbit.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
+
+from . import mdp
+
+##
+# Scene definition
+##s
+
+def SetRigidObjectCfgFromUsdFile(usd_file_name: str):
+    return RigidObjectCfg(
+            prim_path="{ENV_REGEX_NS}/"+(usd_file_name),
+            init_state=RigidObjectCfg.InitialStateCfg(pos=[0.6, 0, 0.055], rot=[1, 0, 0, 0]),
+            spawn=UsdFileCfg(
+                usd_path=f"/home/KTH_dt/usd/"+(usd_file_name)+".usd",
+                scale=(0.01, 0.01, 0.01),
+                rigid_props=RigidBodyPropertiesCfg(
+                solver_position_iteration_count=16,
+                solver_velocity_iteration_count=1,
+                max_angular_velocity=1000.0,
+                max_linear_velocity=1000.0,
+                max_depenetration_velocity=5.0,
+                disable_gravity=False,
+                )
+            )
+        )
