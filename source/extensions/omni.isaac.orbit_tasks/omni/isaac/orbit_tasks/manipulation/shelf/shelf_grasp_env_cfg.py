b1bdeebcb060d9136e53c987f9e82394d21bb5ac
IROL-Kimtaeho <taeho.kim159@gmail.com>
2024-05-21 18:24:37 +0900

    shelf/shelf_grasp_env_cfg 파일 작성 중

---
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/shelf/shelf_grasp_env_cfg.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import MISSING
+
+import torch, numpy as np
+
+import omni.isaac.orbit.sim as sim_utils
+from omni.isaac.orbit.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg
+from omni.isaac.orbit.envs import RLTaskEnvCfg
+from omni.isaac.orbit.managers import CurriculumTermCfg as CurrTerm
+from omni.isaac.orbit.managers import EventTermCfg as EventTerm
+from omni.isaac.orbit.managers import ObservationGroupCfg as ObsGroup
+from omni.isaac.orbit.managers import ObservationTermCfg as ObsTerm
+from omni.isaac.orbit.managers import RewardTermCfg as RewTerm
+from omni.isaac.orbit.managers import SceneEntityCfg
+from omni.isaac.orbit.managers import TerminationTermCfg as DoneTerm
+from omni.isaac.orbit.scene import InteractiveSceneCfg
+from omni.isaac.orbit.sensors import CameraCfg, ContactSensorCfg, RayCasterCfg, patterns 
+from omni.isaac.orbit.sensors.frame_transformer.frame_transformer_cfg import FrameTransformerCfg
+from omni.isaac.orbit.sim.spawners.from_files.from_files_cfg import GroundPlaneCfg, UsdFileCfg
+from omni.isaac.orbit.utils import configclass
+from omni.isaac.orbit.utils.assets import ISAAC_NUCLEUS_DIR
