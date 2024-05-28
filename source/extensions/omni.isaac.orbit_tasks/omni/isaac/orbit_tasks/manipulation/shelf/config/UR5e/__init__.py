0ffd925dfd18ba62ffb7e3fe600affcf1832b3a7
IROL-Kimtaeho <taeho.kim159@gmail.com>
2024-05-28 15:41:32 +0900

    init.py 파일 추가 및  수정

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/shelf/config/UR5e/__init__.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/shelf/config/UR5e/__init__.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import os
 
 from . import agents
-from . import joint_pos_env_cfg
+from .joint_pos_env_cfg import UR5eShelfEnvCfg, UR5eShelfEnvCfg_PLAY
 
 
 ##
@@ -16,21 +16,21 @@ from . import joint_pos_env_cfg
 gym.register(
     id="Isaac-Grasp-Object-UR5e-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
+    disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": joint_pos_env_cfg.UR5eShelfEnvCfg,
+        "env_cfg_entry_point": UR5eShelfEnvCfg,
         # "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.LiftCubePPORunnerCfg,
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
-    disable_env_checker=True,
 )
 
 gym.register(
     id="Isaac-Grasp-Object-UR5e-Play-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
+    disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": joint_pos_env_cfg.UR5eShelfEnvCfg_PLAY,
+        "env_cfg_entry_point": UR5eShelfEnvCfg_PLAY,
         # "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.LiftCubePPORunnerCfg,
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
-    disable_env_checker=True,
 )
