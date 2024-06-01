10927967890ffd832d8b5dd5a8184cb43ae9b41c
Jinseok Kim <jinseok.kim970@gmail.com>
2024-06-01 20:53:20 +0900

    Add & edit observation function
    계산과정 개선 및 이름 수정

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/unstructured/mdp/observations.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/manipulation/unstructured/mdp/observations.py
@@ -9,9 +9,10 @@ import torch
 from typing import TYPE_CHECKING
 
 from omni.isaac.orbit.assets import RigidObject
-from omni.isaac.orbit.managers import SceneEntityCfg
-from omni.isaac.orbit.utils.math import subtract_frame_transforms
+from omni.isaac.orbit.managers import SceneEntityCfg, ManagerTermBase
+from omni.isaac.orbit.utils.math import subtract_frame_transforms, quat_mul, transform_points
 from omni.isaac.orbit.sensors import FrameTransformerData
+from omni.isaac.orbit.managers import ObservationTermCfg as ObsTrem
 
 if TYPE_CHECKING:
     from omni.isaac.orbit.envs import RLTaskEnv
@@ -41,15 +42,104 @@ def object_pose_in_robot_root_frame(
     object: RigidObject = env.scene[object_cfg.name]
     object_pos_w = object.data.root_pos_w[:, :3]
     object_quat_w = object.data.root_quat_w[:, :4]
-    object_pos_b, object_quat_b = subtract_frame_transforms(
-        robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], object_pos_w, object_quat_w
+    object_pos_b, _ = subtract_frame_transforms(
+        robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], object_pos_w
     )
-    return torch.concat((object_pos_b, object_quat_b), dim=1)
+    return torch.concat((object_pos_b, object_quat_w), dim=1)
 
-def eef_pose_in_robot_root_frame(
+class book_flip_point_in_robot_root_frame(ManagerTermBase):
+
+    def __init__(self, cfg: ObsTrem, env: RLTaskEnv):
+        super().__init__(cfg, env)
+
+        asset_cfg = SceneEntityCfg("book_01")
+        self._asset: RigidObject = env.scene[asset_cfg.name]
+
+        # y-up z-center is for grasp-to-flip, x-up z-center is for below-to-flip
+
+        self._top_offset = torch.zeros((env.num_envs, 7), device=env.device)
+        self._top_offset[:,:3] = torch.tensor([0.0, -0.16123+0.03, 0.0127 - 0.03])
+        # self._top_offset[:,3:7] = torch.tensor([0.0, 0.0, -0.70711, -0.70711]) # y-up z-center
+        self._top_offset[:,3:7] = torch.tensor([0.5, -0.5, 0.5, 0.5]) # x-down z-center
+        # self._top_offset[:,3:7] = torch.tensor([0, 0, 90/180*3.141592])
+
+        self._bottom_offset = torch.zeros((env.num_envs, 7), device=env.device)
+        self._bottom_offset[:,:3] = torch.tensor([0.0, 0.16123-0.03, 0.0127 - 0.03])
+        # self._bottom_offset[:,3:7] = torch.tensor([0.70711, 0.70711, 0.0, 0.0]) # y-up z-center
+        self._bottom_offset[:,3:7] = torch.tensor([0.5, 0.5, 0.5, -0.5]) # x-down z-center
+        # self._bottom_offset[:,3:7] = torch.tensor([0, 0, -90/180*3.141592])
+
+        self._left_offset = torch.zeros((env.num_envs, 7), device=env.device)
+        self._left_offset[:,:3] = torch.tensor([0.116881-0.03, 0.0, 0.0127 - 0.03])
+        # self._left_offset[:,3:7] = torch.tensor([0.5, 0.5, -0.5, -0.5]) # y-up z-center
+        self._left_offset[:,3:7] = torch.tensor([0.0, -0.70711, 0.0, 0.70711])  # x-down z-center
+        # self._left_offset[:,3:7] = torch.tensor([0, 0, 3.141592])
+
+        self._right_offset = torch.zeros((env.num_envs, 7), device=env.device)
+        self._right_offset[:,:3] = torch.tensor([-0.116881+0.03, 0.0, 0.0127 - 0.03])
+        # self._right_offset[:,3:7] = torch.tensor([0.5, 0.5, 0.5, 0.5]) # y-up z-center
+        self._right_offset[:,3:7] = torch.tensor([-0.70711, 0.0, -0.70711, 0.0])  # x-down z-center
+        # self._right_offset[:,3:7] = torch.tensor([0, 0, 0])
+
+    
+    def __call__(
+        self,
+        env: RLTaskEnv,
+    ):
+        
+        return self._calc_grasping_pos(env)
+    def _calc_grasping_pos(self,
+                           env: RLTaskEnv
+    ) -> torch.Tensor:
+        # 네 모서리 point의 offset을 book frame에서 simulation world frame 으로 변환하는 quat 적용
+
+        top_pos = torch.cat((transform_points(self._top_offset[:, :3],
+                                              self._asset.data.root_pos_w,
+                                              self._asset.data.root_state_w[:, 3:7])[..., 0 , :],
+                            quat_mul(self._asset.data.root_state_w[:, 3:7],
+                                     self._top_offset[:, 3:7])),
+                            dim=1)
+        bottom_pos = torch.cat((transform_points(self._bottom_offset[:, :3],
+                                                 self._asset.data.root_pos_w,
+                                                 self._asset.data.root_state_w[:, 3:7])[..., 0 , :],
+                                quat_mul(self._asset.data.root_state_w[:, 3:7],
+                                         self._bottom_offset[:, 3:7])),
+                                dim=1)
+        left_pos = torch.cat((transform_points(self._left_offset[:, :3],
+                                               self._asset.data.root_pos_w,
+                                               self._asset.data.root_state_w[:, 3:7])[..., 0, :],
+                            quat_mul(self._asset.data.root_state_w[:, 3:7],
+                                     self._left_offset[:, 3:7])),
+                            dim=1)
+        right_pos = torch.cat((transform_points(self._right_offset[:, :3],
+                                                self._asset.data.root_pos_w,
+                                                self._asset.data.root_state_w[:, 3:7])[..., 0, :],
+                            quat_mul(self._asset.data.root_state_w[:, 3:7],
+                                     self._right_offset[:, 3:7])),
+                            dim=1)
+
+
+        grasp_poses = torch.zeros((env.num_envs, 7), device=env.device)
+
+        poses = torch.stack((top_pos, bottom_pos, left_pos, right_pos), dim=1)
+
+        # Find the index of the maximum z-value for each environment
+        max_indices = torch.argmax(poses[:, :, 2], dim=1)
+
+        # Gather the corresponding poses based on the indices
+        grasp_poses = poses[torch.arange(env.num_envs), max_indices]
+        return grasp_poses
+    
+def eef_pos_in_robot_root_frame(
     env: RLTaskEnv,
 ) -> torch.Tensor:
     """The pose of the end effector in the robot's root frame."""
     ee_tf_data: FrameTransformerData = env.scene["ee_frame"].data
-    
     return ee_tf_data.target_pos_w[..., 0, :]
+
+def eef_quat_in_robot_root_frame(
+    env: RLTaskEnv,
+) -> torch.Tensor:
+    """The pose of the end effector in the robot's root frame."""
+    ee_tf_data: FrameTransformerData = env.scene["ee_frame"].data
+    return ee_tf_data.target_quat_w[..., 0, :]
