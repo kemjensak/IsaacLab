235409a110f91f48339207fd35ca320d6ecbe6d9
IROL-Kimtaeho <taeho.kim159@gmail.com>
2024-06-16 23:27:41 +0900

    shelf align reward 변경, termination 추가

---
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/shelf/mdp/terminations.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING
+
+from omni.isaac.lab.assets import RigidObject
+from omni.isaac.lab.managers import SceneEntityCfg
+from omni.isaac.lab.utils.math import combine_frame_transforms, transform_points
+from omni.isaac.lab.managers import SceneEntityCfg, ManagerTermBase
+from omni.isaac.lab.managers import TerminationTermCfg as DoneTerm
+from omni.isaac.lab.sensors import FrameTransformer
+
+if TYPE_CHECKING:
+    from omni.isaac.lab.envs import ManagerBasedRLEnv
+
+
+def object_reached_goal(
+    env: ManagerBasedRLEnv,
+    command_name: str = "object_pose",
+    threshold: float = 0.02,
+    robot_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+    object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
+) -> torch.Tensor:
+    """Termination condition for the object reaching the goal position.
+
+    Args:
+        env: The environment.
+        command_name: The name of the command that is used to control the object.
+        threshold: The threshold for the object to reach the goal position. Defaults to 0.02.
+        robot_cfg: The robot configuration. Defaults to SceneEntityCfg("robot").
+        object_cfg: The object configuration. Defaults to SceneEntityCfg("object").
+
+    """
+    # extract the used quantities (to enable type-hinting)
+    robot: RigidObject = env.scene[robot_cfg.name]
+    object: RigidObject = env.scene[object_cfg.name]
+    command = env.command_manager.get_command(command_name)
+    # compute the desired position in the world frame
+    des_pos_b = command[:, :3]
+    des_pos_w, _ = combine_frame_transforms(robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], des_pos_b)
+    # distance of the end-effector to the object: (num_envs,)
+    distance = torch.norm(des_pos_w - object.data.root_pos_w[:, :3], dim=1)
+
+    # rewarded if the object is lifted above the threshold
+    return distance < threshold
+
+
+
+class Object_drop_Termination(ManagerTermBase):
+    def __init__(self, cfg: DoneTerm , env: ManagerBasedRLEnv):
+        super().__init__(cfg, env)
+        object_cfg = SceneEntityCfg("cup")
+        ee_frame_cfg = SceneEntityCfg("ee_frame")
+
+        self._target: RigidObject = env.scene[object_cfg.name]
+        self._ee: FrameTransformer = env.scene[ee_frame_cfg.name]
+
+        self._target_last_w = self._target.data.root_pos_w.clone()
+
+        self._top_offset = torch.zeros((env.num_envs, 3), device=env.device)
+        self._top_offset[:, :3] = torch.tensor([0.0, 0.0, 0.07])
+
+    def __call__(self, env:ManagerBasedRLEnv,):
+        drop = self.object_drop(env)
+
+        return drop
+
+    def object_drop(self, env: ManagerBasedRLEnv,)-> torch.Tensor:
+
+        offset_pos = transform_points(self._top_offset,self._target.data.root_pos_w, self._target.data.root_state_w[:, 3:7] )[..., 0 , :]
+        object_vel = self._target.data.root_lin_vel_w
+
+        
+        return offset_pos[:, 2] <  0.7
