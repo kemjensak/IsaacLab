7a0dbcb962b7e4a783bd40e44c795f7fdd94f124
IROL-Kimtaeho <taeho.kim159@gmail.com>
2024-06-18 15:49:29 +0900

    shelf reach reward 수정

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/shelf/mdp/rewards.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/shelf/mdp/rewards.py
@@ -26,8 +26,6 @@ class shelf_Reaching(ManagerTermBase):
         self._shelf: RigidObject = env.scene[shelf_cfg.name]
 
         self.__initial_object_pos = self._target.data.root_pos_w.clone()
-        self.__initial_object_pos[:, 2] = self.__initial_object_pos[:, 2] + 0.09
-        self.__initial_object_pos[:, 1] = self.__initial_object_pos[:, 1] + 0.08
         self._initial_shelf_pos = self._shelf.data.root_pos_w.clone()
         self._initial_ee_quat = self._ee.data.target_quat_w.clone()
 
@@ -36,27 +34,30 @@ class shelf_Reaching(ManagerTermBase):
         self._ee_pos_last_w = self._ee.data.target_pos_w[..., 0, :].clone()
 
         self._target_last_w = self._target.data.root_pos_w.clone()
+        self._reach_offset = torch.zeros((env.num_envs, 3), device=env.device)
+        self._reach_offset[:, :3] = torch.tensor([0.06, 0.12, 0.04])
     
     def __call__(self, env: ManagerBasedRLEnv,):
 
         reach = self.object_ee_distance(env)
         align = self.align_ee_target(env)
 
-        return align
+        return reach + align
     
     def object_ee_distance(self, env:ManagerBasedRLEnv) -> torch.Tensor:
-        ready_point_pos_w = self._target.data.root_pos_w.clone()
-        ready_point_pos_w[:, 2] = ready_point_pos_w[:, 2] + 0.09
-        ready_point_pos_w[:, 1] = ready_point_pos_w[:, 1] + 0.08
+        # ee target position
+        offset_pos = transform_points(self._reach_offset,self._target.data.root_pos_w, self._target.data.root_state_w[:, 3:7] )[..., 0 , :]
 
-        distance = torch.norm(ready_point_pos_w - self._ee.data.target_pos_w[..., 0, :], dim=-1, p=2)
-        reset_mask = env.episode_length_buf == 1
+        distance = torch.norm(offset_pos - self._ee.data.target_pos_w[..., 0, :], dim=-1, p=2)
+        # reset_mask = env.episode_length_buf == 1
         
-        self._initial_distance[reset_mask] = distance[reset_mask].clone()
-        self._ee_pos_last_w = self._ee.data.target_pos_w[..., 0, :].clone()
+        # self._initial_distance[reset_mask] = distance[reset_mask].clone()
+        # self._ee_pos_last_w = self._ee.data.target_pos_w[..., 0, :].clone()
         
 
         # print("distance: {}".format(distance))
+        # print("ee: {}".format(self._ee.data.target_pos_w[..., 0, :]))
+        # print("target: {}".format(offset_pos))
         reward = torch.exp(-1.2 * distance)
         
         return reward
@@ -104,7 +105,7 @@ class shelf_Pushing(ManagerTermBase):
         self._target_last_w = self._target.data.root_pos_w.clone()
 
         self._push_offset = torch.zeros((env.num_envs, 3), device=env.device)
-        self._push_offset[:, :3] = torch.tensor([0.06, 0.15, 0.04])
+        self._push_offset[:, :3] = torch.tensor([0.06, 0.12, 0.04])
     
     def __call__(self, env: ManagerBasedRLEnv,):
 
@@ -116,6 +117,7 @@ class shelf_Pushing(ManagerTermBase):
 
         # current object state
         object_pos_w = self._target.data.root_pos_w.clone()
+
         # current ee state
         ee_pos_w = self._ee.data.target_pos_w.clone()
 
@@ -129,11 +131,7 @@ class shelf_Pushing(ManagerTermBase):
         
         # distance between ee and object
         distance = torch.norm(offset_pos - self._ee.data.target_pos_w[..., 0, :], dim=-1, p=2)
-        distance_f = torch.norm(self._initial_object_pos - self._ee.data.target_pos_w[..., 0, :],  dim=-1, p=2)
-        # print("dis: {}".format(distance))
-        # print("obj: {}".format(offset_pos))
-        # print("tcp: {}".format(self._ee.data.target_pos_w[..., 0, :]))
-        
+        distance_f = torch.norm(self._initial_object_pos - self._ee.data.target_pos_w[..., 0, :],  dim=-1, p=2)   
 
         # Displacement of end-effector from initial state
         D_y_ee = (ee_pos_w[..., 0, 1] - self._initial_ee_pos[..., 0, 1])
@@ -147,24 +145,32 @@ class shelf_Pushing(ManagerTermBase):
 
         # Displacement of cup from initial state
         delta_y = -1*(object_pos_w[:, 1] - self._initial_object_pos[:, 1])
-        # delta_x = torch.where(torch.norm(object_pos_w[:, 0] - self._target_last_w[:, 0])>0.1, 1, 0)
+        delta_x = (object_pos_w[:, 0] - self._initial_object_pos[:, 0])
         # delta_z = torch.where(torch.norm(object_pos_w[:, 2] - self._target_last_w[:, 2])>0.1, 1, 0)
 
         # indicator factor
         zeta_s = torch.where(torch.abs(object_pos_w[:, 1] - self._initial_object_pos[:, 1]) > 0.19, 0, 1)
         zeta_m = torch.where(torch.bitwise_or(distance < 0.03, torch.abs(object_pos_w[:, 1] - self._initial_object_pos[:, 1]) > 0.19) , 1, 0)
 
-        R = zeta_m * ((3*torch.tanh(2*delta_y/0.2)) - 0.2 * (torch.tanh(2 * D_x_ee/0.1)) + velocity_reward) + 4*(1-zeta_s)*torch.exp(-1.2 * distance_f)
+        velocity_reward = zeta_s * torch.where(v_y_ee < 0.45, v_y_ee * 3, -3 * v_y_ee)
+
+        pushing_reward = zeta_m * ((3*torch.tanh(2*delta_y/0.2)) - 0.1 * (torch.tanh(2 * D_x_ee/0.1)) + velocity_reward)
+
+        pushing_reward = torch.clamp(pushing_reward, -4, 4)
 
 
-        # R = zeta_s * zeta_m * (5*torch.tanh.(2*delta_y) - 0.05*torch.tanh(2*delta_x)-torch.tanh(2*delta_z)) + (1-zeta_s)*(1-distance_f/self._initial_distance)
-        # print("distance: {}".format(distance))
+        R = pushing_reward + (1-zeta_s)*3*torch.exp(-1.2 * distance_f)
+
+        # print("distance: {}".format(distance_f))
         # print("delta_y: {}".format(delta_y))
+        # print("delta_x: {}".format(delta_x))
+        # print(4*torch.exp(-1.2 * distance_f))
         # print("ee: {}".format( self._ee.data.target_pos_w[..., 0, :]))
         # print("object: {}".format(offset_pos))
         # print("zeta_m: {}".format(zeta_m))
         # print("zeta_s: {}".format(zeta_s))
-        # print("reward: {}".format(R))
+        # print("reward: {}".format(pushing_reward))
+        # print("vel rew: {}".format(velocity_reward))
         # print("dt: {}".format(delta_y_ee/env.step_dt))
         # self._target_last_w = object_pos_w.clone()
 
